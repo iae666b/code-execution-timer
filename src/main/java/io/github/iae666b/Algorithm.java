3318d8eea8fa8d1bef7814f3928efe5d8e702bf9
iae666b <iae666@foxmail.com>
2024-08-08 09:53:17 +0800

    Init

---
+++ src/main/java/io/github/iae666b/Algorithm.java
@@ -0,0 +1,11 @@
+package io.github.iae666b;
+
+/**
+ * Abstraction of specific algorithms.
+ */
+public abstract class Algorithm {
+    /**
+     * Storing the specific logic of the algorithm
+     */
+    public abstract void execute();
+}
---
+++ src/main/java/io/github/iae666b/ExecutionTimer.java
@@ -0,0 +1,22 @@
+package io.github.iae666b;
+
+/**
+ * The core class for calculating the running time of a specific algorithm.
+ */
+public class ExecutionTimer {
+    /**
+     * Returns the execution time of algorithms in milliseconds.
+     *
+     * @param algorithm the specific algorithm
+     * @return execution time of algorithms in milliseconds
+     */
+    public static long calculate(Algorithm algorithm) {
+        long start = System.currentTimeMillis();
+        algorithm.execute();
+        long end = System.currentTimeMillis();
+        
+        long elapsedTime = end - start;
+        System.out.println("Execution time: " + elapsedTime + " ms");
+        return elapsedTime;
+    }
+}
