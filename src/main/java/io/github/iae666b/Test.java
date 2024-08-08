6b2788167038cba5f1a951f760f09b9f81ac1958
iae666b <iae666@foxmail.com>
2024-08-08 09:55:48 +0800

    Add normal test

---
+++ src/main/java/io/github/iae666b/Test.java
@@ -0,0 +1,15 @@
+package io.github.iae666b;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutionTimer.calculate(new Algorithm() {
+            @Override
+            public void execute() {
+                int sum = 0;
+                for (int i = 0; i < 10000; i++) {
+                    sum += i;
+                }
+            }
+        });
+    }
+}
